c6958c9064abca746e0f002e746328e44c2c8ea2
Xavier Alexandre <poloderty21@gmail.com>
2020-02-24 22:51:29 -0500

    Finalisation de la section skills avec enfin une version responsive pour chaque écran

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -14,30 +14,28 @@ function Bio() {
   return (
     <section id="bio" className="text-center">
       <Row>
-        <Col id="intro" xs="12">
+        <Col id="intro">
           <h2>Découvrez le Xavier d&apos;Amérique!</h2>
           <p>Une créature appréciée de tous</p>
         </Col>
       </Row>
-      <Row className="maxWidth maxHeight">
-        <Col id="presentation" xs="12" lg="6">
+      <Row id="presentation" className="maxWidth">
+        <Col>
           <Image fluid src={barbueVert} alt="Barbue sur rond au fond vert" />
           <h3>Xavier Alexandre</h3>
           <h4>Intégrateur Web / Développeur Front End</h4>
           <h6>(et magnifique personne)</h6>
         </Col>
-        <Col id="skills" xs="12" lg="6">
-          <Row>
-            <h3 className="mx-auto">Capacitiés</h3>
-          </Row>
-          <Skills />
-          <Row>
-            <h4 className="mx-auto">Il est habile dans tous ces domaines!</h4>
-          </Row>
+      </Row>
+      <Row id="skills" className="maxWidth">
+        <Col>
+          <h3 className="">Capacitiés</h3>
+          <h4 className="">Il est habile dans tous ces domaines, certains plus que d&apos;autres :</h4>
+          <h6>Observez les contours</h6>
         </Col>
+        <Skills />
       </Row>
     </section>
-
   );
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,65 +1,235 @@
 import React from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
+/* Languages */
 import html from '../media/skill-logo/HTML5.png';
 import css from '../media/skill-logo/CSS3.png';
-import git from '../media/skill-logo/Git.png';
+import js from '../media/skill-logo/JS.png';
 import sass from '../media/skill-logo/Sass.png';
-import react from '../media/skill-logo/React.png';
+import jquery from '../media/skill-logo/jQuery.png';
+import php from '../media/skill-logo/PHP.png';
+import csharp from '../media/skill-logo/CSharp.png';
+import java from '../media/skill-logo/Java.png';
+import mysql from '../media/skill-logo/MySQL.png';
+
+/* Logiciel */
+import photoshop from '../media/skill-logo/Photoshop.png';
+import illustrator from '../media/skill-logo/Illustrator.png';
+import indesign from '../media/skill-logo/InDesign.png';
+import animate from '../media/skill-logo/Animate.png';
+import premiere from '../media/skill-logo/PremierePro.png';
+import aftereffect from '../media/skill-logo/AfterEffect.png';
+import logic from '../media/skill-logo/Logic.png';
 import unity from '../media/skill-logo/Unity.png';
+import maya from '../media/skill-logo/Maya.png';
+
+/* Util */
+import git from '../media/skill-logo/Git.png';
+import react from '../media/skill-logo/React.png';
+import wordpress from '../media/skill-logo/Wordpress.png';
+import phaser from '../media/skill-logo/Phaser.png';
+import angular from '../media/skill-logo/Angular.png';
+import node from '../media/skill-logo/Node.png';
+import npm from '../media/skill-logo/npm.png';
+import bootstrap from '../media/skill-logo/Bootstrap.png';
+import ajax from '../media/skill-logo/AJAX.png';
 
 
 import './_Skills.scss';
 
 const skills = [
-  {
-    name: 'Html5',
-    url: html,
-  },
-  {
-    name: 'Css3',
-    url: css,
-  },
-  {
-    name: 'Git',
-    url: git,
-  },
-  {
-    name: 'Sass',
-    url: sass,
-  },
-  {
-    name: 'ReactJs',
-    url: react,
-  },
-  {
-    name: 'Unity',
-    url: unity,
-  },
-  {
-    name: 'Maya',
-    url: '',
-  },
-
+  [
+    {
+      name: 'HTML5',
+      pourcent: 90,
+      url: html,
+    },
+    {
+      name: 'CSS3',
+      pourcent: 90,
+      url: css,
+    },
+    {
+      name: 'Javascipt',
+      pourcent: 80,
+      url: js,
+    },
+    {
+      name: 'Sass',
+      pourcent: 80,
+      url: sass,
+    },
+    {
+      name: 'jQuery',
+      pourcent: 60,
+      url: jquery,
+    },
+    {
+      name: 'PHP',
+      pourcent: 80,
+      url: php,
+    },
+    {
+      name: 'C#',
+      pourcent: 60,
+      url: csharp,
+    },
+    {
+      name: 'Java',
+      pourcent: 60,
+      url: java,
+    },
+    {
+      name: 'MySQL',
+      pourcent: 80,
+      url: mysql,
+    },
+  ],
+  [
+    {
+      name: 'Photoshop',
+      pourcent: 80,
+      url: photoshop,
+    },
+    {
+      name: 'Illustrator',
+      pourcent: 80,
+      url: illustrator,
+    },
+    {
+      name: 'Premiere',
+      pourcent: 80,
+      url: premiere,
+    },
+    {
+      name: 'After Effect',
+      pourcent: 80,
+      url: aftereffect,
+    },
+    {
+      name: 'InDesign',
+      pourcent: 60,
+      url: indesign,
+    },
+    {
+      name: 'Animate',
+      pourcent: 60,
+      url: animate,
+    },
+    {
+      name: 'Unity',
+      pourcent: 60,
+      url: unity,
+    },
+    {
+      name: 'Maya',
+      pourcent: 50,
+      url: maya,
+    },
+    {
+      name: 'Logic Pro',
+      pourcent: 60,
+      url: logic,
+    },
+  ],
+  [
+    {
+      name: 'React',
+      pourcent: 60,
+      url: react,
+    },
+    {
+      name: 'Angular',
+      pourcent: 50,
+      url: angular,
+    },
+    {
+      name: 'Wordpress',
+      pourcent: 60,
+      url: wordpress,
+    },
+    {
+      name: 'Git',
+      pourcent: 90,
+      url: git,
+    },
+    {
+      name: 'NodeJs',
+      pourcent: 60,
+      url: node,
+    },
+    {
+      name: 'npm',
+      pourcent: 90,
+      url: npm,
+    },
+    {
+      name: 'Bootstrap',
+      pourcent: 90,
+      url: bootstrap,
+    },
+    {
+      name: 'Phaser',
+      pourcent: 80,
+      url: phaser,
+    },
+    {
+      name: 'AJAX',
+      pourcent: 90,
+      url: ajax,
+    },
+  ],
 ];
 
+function getDegValue(pourcent) {
+  return (pourcent * 360) / 100;
+}
 
 function Skills() {
   return (
-    <Row id="skill-tree">
-      {skills.map(({ name, url }, id) => (
-        <Col key={name} xs="12" className="skill">
-          <span id={id} className="skill-title">{name}</span>
-          <div className="ligne-centre" />
-          <div className="skill-div-logo">
-            <div className="skill-logo">
-              <img src={url} alt="" />
+    <div id="skill-tree">
+      <div id="skill-lang">
+        {skills[0].map(({ name, pourcent, url }, id) => (
+          <div key={name} className="skill">
+            <div className="skill-container">
+              <div className="skill-pourcent" style={{ backgroundImage: `linear-gradient(0deg, transparent 50%, #277b32 50%), linear-gradient(${getDegValue(pourcent)}deg, #277b32 50%, transparent 50%)` }}>
+                <div className="skill-logo">
+                  <img src={url} alt="" />
+                </div>
+              </div>
             </div>
+            <span id={id} className="skill-title">{name}</span>
           </div>
-        </Col>
-      ))}
-    </Row>
+        ))}
+      </div>
+      <div id="skill-util">
+        {skills[1].map(({ name, pourcent, url }, id) => (
+          <div key={name} className="skill">
+            <div className="skill-container">
+              <div className="skill-pourcent" style={{ backgroundImage: `linear-gradient(0deg, transparent 50%, #277b32 50%), linear-gradient(${getDegValue(pourcent)}deg, #277b32 50%, transparent 50%)` }}>
+                <div className="skill-logo">
+                  <img src={url} alt="" />
+                </div>
+              </div>
+            </div>
+            <span id={id} className="skill-title">{name}</span>
+          </div>
+        ))}
+      </div>
+      <div id="skill-util">
+        {skills[2].map(({ name, pourcent, url }, id) => (
+          <div key={name} className="skill">
+            <div className="skill-container">
+              <div className="skill-pourcent" style={{ backgroundImage: `linear-gradient(0deg, transparent 50%, #277b32 50%), linear-gradient(${getDegValue(pourcent)}deg, #277b32 50%, transparent 50%)` }}>
+                <div className="skill-logo">
+                  <img src={url} alt="" />
+                </div>
+              </div>
+            </div>
+            <span id={id} className="skill-title">{name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
