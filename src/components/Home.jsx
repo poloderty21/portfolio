3d9dcf2a6768c51dcaca446c89790c0a2da325b0
Xavier Alexandre <poloderty21@gmail.com>
2020-02-25 23:07:09 -0500

    Ajout de la page home pour faire fonctionner le router

---
+++ src/components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from './Header';
+import Bio from './Bio';
+import Ligne from './Ligne';
+import Portfolio from './Portfolio';
+import Footer from './Footer';
+
+import './_Home.scss';
+
+function Home() {
+  return (
+    <>
+      <Header name="Xavier" />
+      {/* <Link to="/users">Home</Link> */}
+      <Bio />
+      <Portfolio />
+      <Ligne />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
